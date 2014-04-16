7cb34fd3335a5c9267816459b77e4c481a0d5f80
Matt <matthewgorelik@gmail.com>
2014-04-16 15:29:15 -0400

    Added onItemClickListener to RxFragment...still must add functionality

--- Refill/src/cs250/spring14/refill/RxFragment.java
+++ Refill/src/cs250/spring14/refill/RxFragment.java
@@ -1,15 +1,17 @@
 package cs250.spring14.refill;
 
 import java.text.ParseException;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class RxFragment extends Fragment {
 	ListView rxList;
@@ -26,6 +28,17 @@ public class RxFragment extends Fragment {
 			e.printStackTrace();
 		}
         rxList.setAdapter(rxAdap);
+        rxList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				//This is where we will add functionality to edit the Rx
+				RxItem rx = (RxItem) parent.getItemAtPosition(position);
+				Toast.makeText(getActivity(), "You clicked on " + rx.getName(), Toast.LENGTH_SHORT).show();
+			}
+        	
+        });
         rxAdap.notifyDataSetChanged();
         return rootView;
     }
