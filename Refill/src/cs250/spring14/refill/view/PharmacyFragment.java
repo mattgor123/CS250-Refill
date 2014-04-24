7ed10404124f1fa9ac6150b595a6cfd9c80ebafc
Matt <matthewgorelik@gmail.com>
2014-04-24 04:04:08 -0400

    Quick fix in PharmacyFragment

--- Refill/src/cs250/spring14/refill/view/PharmacyFragment.java
+++ Refill/src/cs250/spring14/refill/view/PharmacyFragment.java
@@ -6,22 +6,14 @@ import cs250.spring14.refill.MainActivity;
 import cs250.spring14.refill.R;
 import cs250.spring14.refill.core.HistoryItem;
 import cs250.spring14.refill.core.Pharmacy;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
@@ -65,7 +57,6 @@ public class PharmacyFragment extends DialogFragment implements RefreshableFragm
 							@Override
 							public void onClick(DialogInterface dialog,
 									int which) {
-								// Remove functionality must be added here
 								if (ph != null
 										&& MainActivity.rxAdapter.existsRxWithPharm(Pharmacy
 												.makeStringFromPharm(ph))) {
@@ -103,7 +94,7 @@ public class PharmacyFragment extends DialogFragment implements RefreshableFragm
 							public void onClick(DialogInterface dialog,
 									int which) {
 								if (ph != null) {
-									openEditPharmacyDialog(getActivity(), ph, PharmacyFragment.this);
+									Pharmacy.openEditPharmacyDialog(getActivity(), ph, PharmacyFragment.this);
 								} 
 							}
 						});
@@ -121,116 +112,6 @@ public class PharmacyFragment extends DialogFragment implements RefreshableFragm
 		repopulateAdapter();
 	}
 	
-	/**
-	 * Helper method to open edit pharmacy dialog
-	 * 
-	 * @param context
-	 *            the context for the dialog
-	 * @param ph
-	 *            the pharmacy to edit
-	 */
-	public static void openEditPharmacyDialog(final Context context,
-			final Pharmacy ph, final RefreshableFragment fr) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		LinearLayout layout = new LinearLayout(context);
-		layout.setOrientation(1);
-		// TODO Auto-generated method stub
-		final EditText name = new EditText(context);
-		final EditText email = new EditText(context);
-		final EditText phone = new EditText(context);
-		final EditText address = new EditText(context);
-		Button add = new Button(context);
-		add.setText("Update This Pharmacy/Exit Dialog");
-		name.setSingleLine();
-		email.setSingleLine();
-		phone.setSingleLine();
-		address.setSingleLine();
-		name.setHint("    Name (must be unique): ");
-		email.setHint("    Email: ");
-		phone.setHint("    Phone: ");
-		address.setHint("    Street (123 Oak): ");
-		name.setText(ph.getName());
-		email.setText(ph.getEmail());
-		phone.setText(ph.getPhone());
-		address.setText(ph.getStreetAddress());
-		name.setInputType(InputType.TYPE_CLASS_TEXT
-				| InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
-		email.setInputType(InputType.TYPE_CLASS_TEXT
-				| InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-		phone.setInputType(InputType.TYPE_CLASS_NUMBER);
-		address.setInputType(InputType.TYPE_CLASS_TEXT
-				| InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS);
-		layout.addView(name);
-		layout.addView(email);
-		layout.addView(phone);
-		layout.addView(address);
-		layout.addView(add);
-		builder.setView(layout);
-		final Dialog d = builder.create();
-		add.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// Basic input checking; will get better with time
-				String nameStr = name.getText().toString().trim();
-				String emailStr = email.getText().toString().trim();
-				String phoneStr = phone.getText().toString().trim();
-				String addressStr = address.getText().toString().trim();
-				if (!MainActivity.isValidName(nameStr)) {
-					Toast.makeText(context,
-							"Please ensure you've entered a valid name",
-							Toast.LENGTH_SHORT).show();
-				} else if (!MainActivity.isValidEmail(emailStr)) {
-					Toast.makeText(context,
-							"Please ensure you've entered a valid email",
-							Toast.LENGTH_SHORT).show();
-				} else if (!MainActivity.isValidPhone(phoneStr)) {
-					Toast.makeText(context,
-							"Please ensure you've entered a valid phone",
-							Toast.LENGTH_SHORT).show();
-				} else if (!MainActivity.isValidStreet(addressStr)) {
-					Toast.makeText(
-							context,
-							"Please ensure you've entered a valid street address",
-							Toast.LENGTH_SHORT).show();
-				} else if (Pharmacy.shouldUpdatePharm(ph, nameStr, emailStr, phoneStr,
-						addressStr)) {
-					if (MainActivity.phAdapter.updatePh(ph.getId(), nameStr,
-							emailStr, phoneStr, addressStr)) {
-						// We successfully updated the Doctor
-						String oldPharm = Pharmacy.makeStringFromPharm(ph);
-						ph.setName(nameStr);
-						ph.setEmail(emailStr);
-						ph.setPhone(phoneStr);
-						ph.setStreetAddress(addressStr);
-						String newPharm = Pharmacy.makeStringFromPharm(ph);
-						MainActivity.rxAdapter.updateAllRxWithPharmacy(
-								oldPharm, newPharm);
-						String message = "Updated in Pharmacy DB on "
-								+ MainActivity.df.format(Calendar.getInstance()
-										.getTime());
-						HistoryItem his = new HistoryItem(
-								nameStr, message, "P");
-						MainActivity.hAdapter.insertHis(his);
-						//hisAdap.add(his);
-						//hisAdap.notifyDataSetChanged();
-						fr.repopulateAdapter();
-						d.dismiss();
-					} else {
-						// We didn't actually successfully update the Doctor
-						String message = "Something went wrong updating your Pharmacy. Perhaps a Pharmacy with the name already exists?";
-						Toast.makeText(context, message, Toast.LENGTH_SHORT)
-								.show();
-					}
-				} else {
-					// We shouldn't update; were just viewing
-					d.dismiss();
-				}
-			}
-		});
-		d.show();
-	}
-
 	/**
 	 * Method to repopulate the array adapter with changes produced in the edit dialog
 	 */
