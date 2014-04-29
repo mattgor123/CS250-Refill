48fee3d385e82d86e7119d953d33dd51f560ac61
Fernando Jose Ribeiro de Brito <friberi1@jhu.edu>
2014-04-28 23:33:31 -0400

    Schedule Dialog

--- Refill/src/cs250/spring14/refill/view/ScheduleAdapter.java
+++ Refill/src/cs250/spring14/refill/view/ScheduleAdapter.java
@@ -21,8 +21,8 @@ public class ScheduleAdapter extends BaseAdapter{
 
 	private Context mContext;
 	private Calendar cal;
-	private String[] days = new String[]{"", "Sun", " M ", " T ", " W ", " Th", " F ", "Sat"};
-	private String[] hours = new String[]{"", " 6AM", " 7AM", " 8AM", " 9AM", "10AM", "11AM", "12PM",
+	private String[] days = new String[]{"", "| Sun |", "|   M  |", "|   T   |", "|   W  |", "|  Th  |", "|   F   |", "|  Sat |"};
+	private String[] hours = new String[]{"", " 6AM ", " 7AM ", " 8AM ", " 9AM ", "10AM", "11AM", "12PM",
 			" 1PM", " 2PM", " 3PM", " 4PM", " 5PM", " 6PM", " 7PM", " 8PM", " 9PM", "10PM"};
 	
     public ScheduleAdapter(Context c) {
@@ -92,7 +92,7 @@ public class ScheduleAdapter extends BaseAdapter{
 			v.setVisibility(View.VISIBLE);
 			layout = (LinearLayout) v.findViewById(R.id.schedule_layout);
 			TextView txt = new TextView(mContext);
-			txt.setText("|      |");
+			txt.setText("|         |");
 			txt.setLines(1);
 			layout.addView(txt);
 		}
--- Refill/src/cs250/spring14/refill/view/ScheduleFragment.java
+++ Refill/src/cs250/spring14/refill/view/ScheduleFragment.java
@@ -1,5 +1,7 @@
 package cs250.spring14.refill.view;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -7,9 +9,14 @@ import cs250.spring14.refill.MainActivity;
 import cs250.spring14.refill.R;
 import cs250.spring14.refill.core.Doctor;
 import cs250.spring14.refill.core.HistoryItem;
+import cs250.spring14.refill.core.Patient;
+import cs250.spring14.refill.core.RxItem;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -17,21 +24,30 @@ import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class ScheduleFragment extends DialogFragment {
 	
 	GridView calendar;
 	ScheduleAdapter scheduleAdap;
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		getDialog().setTitle("Schedule");
@@ -47,11 +63,70 @@ public class ScheduleFragment extends DialogFragment {
 
 		calendar.setOnItemClickListener(new OnItemClickListener() {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	            Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+	        	// we shouldn't do anything with the first row and first column 
+	        	if (position > 8 && position%8 != 0){
+	        		openScheduleRxDialog(getActivity(), v);
+	        		//Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+		            //v.setBackgroundColor(-13159);
+	        	}
 	        }
 	    });
 
 		
 		return rootView;
 	}
+	
+	protected void openScheduleRxDialog(final Context context, final View parent){
+	    final Dialog dialog = new Dialog(context);
+	    int color;
+	    dialog.setTitle("Please add a patient");
+	    dialog.setContentView(R.layout.schedule_dialog);
+	    final Spinner spinner = (Spinner) dialog.findViewById(R.id.Rx_Spinner);
+	    Button ok = (Button) dialog.findViewById(R.id.ok);
+	    ArrayList<RxItem> rxList;
+		try {
+			rxList = MainActivity.rxAdapter.getAllRxs();
+			final ArrayAdapter<RxItem> aa =
+			        new ArrayAdapter<RxItem>(getActivity(), android.R.layout.simple_spinner_item, rxList);
+			    
+			    spinner.setAdapter(aa);
+			    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			        @Override
+			        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			          RxItem rx = (RxItem) spinner.getSelectedItem();
+			          if (rx.getId() == 1) {
+			            // We hit our Dummy Doctor, do nothing.
+			          } else {
+			            //color = rx.getPatient().getColor();
+			            //return color;
+			            //dialog.dismiss();
+			          }
+			        }
+
+			        @Override
+			        public void onNothingSelected(AdapterView<?> parent) {
+			          // Nothing selected; do nothing
+			        }
+
+			      });
+			    ok.setOnClickListener(new OnClickListener() {
+			      @Override
+			      public void onClick(View v) {
+			    	  if (spinner.getSelectedItem() != null){
+			    		  parent.setBackgroundColor(((RxItem)spinner.getSelectedItem()).getPatient().getColor());
+			    		  TextView txt = new TextView(getActivity());
+			    		  txt.setText(((RxItem)spinner.getSelectedItem()).getName().substring(0, 2));
+			    		  ((LinearLayout)parent).addView(txt);
+			  	          dialog.dismiss();
+			    	  }
+			      }
+			    });
+			    dialog.show();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+		}
+	    
+	  }
 }
