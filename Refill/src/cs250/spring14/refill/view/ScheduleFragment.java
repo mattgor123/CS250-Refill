b7ff10b9388c4e984690cc9d20dc1a0331e11ae8
Fernando Jose Ribeiro de Brito <friberi1@jhu.edu>
2014-04-29 03:57:30 -0400

    Schedule fully working

--- Refill/src/cs250/spring14/refill/view/ScheduleFragment.java
+++ Refill/src/cs250/spring14/refill/view/ScheduleFragment.java
@@ -11,8 +11,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,16 +20,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 public class ScheduleFragment extends DialogFragment implements RefreshableFragment{
 	GridView grid;
-	ScheduleAdapter scheduleAdap;
+	ScheduleWrapper scheduleAdap;
 	
 	private RefreshableFragment.OnCompleteListener mListener;
 
@@ -61,7 +57,7 @@ public class ScheduleFragment extends DialogFragment implements RefreshableFragm
 		
 		grid = (GridView) rootView.findViewById(R.id.gridView1);
 		
-		scheduleAdap = new ScheduleAdapter(rootView.getContext(), 0, MainActivity.scAdapter.getAllSchs());
+		scheduleAdap = new ScheduleWrapper(rootView.getContext(), 0, MainActivity.scAdapter.getAllSchs());
 		repopulateAdapter();
 		grid.setAdapter(scheduleAdap);
 
@@ -75,35 +71,32 @@ public class ScheduleFragment extends DialogFragment implements RefreshableFragm
 	        			openScheduleRxDialog(getActivity(), v, false, position); 
 	        		}else{
 	        			openScheduleRxDialog(getActivity(), v, true, position);
-	        		}
-	        		for (int i = 0; i < ((LinearLayout)v).getChildCount(); i++){
-	        			TextView txt2 = (TextView) ((LinearLayout)v).getChildAt(i);
-	        			Toast.makeText(getActivity(), txt2.getText(),Toast.LENGTH_SHORT).show();
-	        		}        		
+	        		}     		
 	        	}
-	        		
-	        		//Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
-		            //v.setBackgroundColor(-13159);
 	        }
 	    });
 		return rootView;
 	}
 	
+
 	protected void openScheduleRxDialog(final Context context, final View parent, final boolean hasPrescription, final int position){
 	    final Dialog dialog = new Dialog(context);
 	    dialog.setTitle("Select Rx");
 	    dialog.setContentView(R.layout.schedule_dialog);
+	    final LinearLayout ll = (LinearLayout) dialog.findViewById(R.id.rxs_layout);
 	    final Spinner spinner = (Spinner) dialog.findViewById(R.id.Rx_Spinner);
-	    ListView rxListView = (ListView) dialog.findViewById(R.id.Rx_list);
 	    Button ok = (Button) dialog.findViewById(R.id.ok);
+	    final ArrayList<ScheduleItem> schList = MainActivity.scAdapter.getScshByPos(position);
 	    
 	    if (hasPrescription)
 	    {
-	    	for (int i = 0; i < ((LinearLayout)parent).getChildCount(); i++){
-    			TextView txt = (TextView) ((LinearLayout)parent).getChildAt(i);
-    			
-    			//Toast.makeText(getActivity(), txt2.getText(),Toast.LENGTH_SHORT).show();
-    		}
+	    	for(ScheduleItem s : schList)
+	    	{
+	    		TextView txt = new TextView (context);
+	    		txt.setText(s.getName());
+	    		txt.setBackgroundColor(s.getColor());
+	    		ll.addView(txt);
+	    	}
 	    }
 	    
 	    ArrayList<RxItem> rxList;
@@ -112,50 +105,28 @@ public class ScheduleFragment extends DialogFragment implements RefreshableFragm
 			final ArrayAdapter<RxItem> aa =
 			        new ArrayAdapter<RxItem>(getActivity(), android.R.layout.simple_spinner_item, rxList);
 			    spinner.setAdapter(aa);
-			    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			        @Override
-			        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-			          RxItem rx = (RxItem) spinner.getSelectedItem();
-			          if (rx.getId() == 1) {
-			            // We hit our Dummy Doctor, do nothing.
-			          } else {
-			            //color = rx.getPatient().getColor();
-			            //return color;
-			            //dialog.dismiss();
-			          }
-			        }
-
-			        @Override
-			        public void onNothingSelected(AdapterView<?> parent) {
-			          // Nothing selected; do nothing
-			        }
-
-			      });
 			    ok.setOnClickListener(new OnClickListener() {
 			      @Override
 			      public void onClick(View v) {
 			    	  if (hasPrescription){
+			    		  RxItem r = (RxItem)spinner.getSelectedItem();
+			    		  for(ScheduleItem s : schList){
+			    			  if (s.getName().equals(r.getName()) && s.getColor() == r.getPatient().getColor())
+			    			  {
+			    				  dialog.dismiss();
+				    			  return; 
+			    			  }
+			    		  }
 			    		  MainActivity.scAdapter.insertSch(new ScheduleItem(((RxItem)spinner.getSelectedItem()).getName(),position, 
 			    				  ((RxItem)spinner.getSelectedItem()).getPatient().getColor()));
 			    		  repopulateAdapter();
-			    		  Toast.makeText(getActivity(), MainActivity.scAdapter.getSize()+" ", Toast.LENGTH_SHORT).show();
-			    		  /*TextView txt = new TextView(getActivity());
-			    		  txt.setBackgroundColor(((RxItem)spinner.getSelectedItem()).getPatient().getColor());
-			    		  txt.setText(((RxItem)spinner.getSelectedItem()).getName());
-			    		  txt.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-			    		  txt.setSingleLine();
-			    		  txt.setEllipsize(TextUtils.TruncateAt.END);
-			    		  ((LinearLayout)parent).addView(txt);*/
 			    	  }
 			    	  else{
+			    		  // removing the pipes TextView
 			    		  ((LinearLayout)parent).removeAllViews();
 			    		  MainActivity.scAdapter.insertSch(new ScheduleItem(((RxItem)spinner.getSelectedItem()).getName(),position, 
 			    				  ((RxItem)spinner.getSelectedItem()).getPatient().getColor()));
 			    		  repopulateAdapter();
-			    		  
-			    		  Toast.makeText(getActivity(), MainActivity.scAdapter.getSize()+" ", Toast.LENGTH_SHORT).show();
-			    		  //((LinearLayout)parent).addView(txt);
 			    	  }
 			  	      dialog.dismiss();
 			      }
@@ -167,7 +138,7 @@ public class ScheduleFragment extends DialogFragment implements RefreshableFragm
 		}
 	    
 	  }
-
+	
 	@Override
 	public void repopulateAdapter() {
 		// TODO Auto-generated method stub
